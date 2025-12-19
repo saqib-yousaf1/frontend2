90d65f837ff6fa6e67ebacd89cf6eb013e470b06
Hafsa Abid <hafsaabd.82@gmail.com>
2025-12-19 14:05:43 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,8 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 
-import "./App.css";
-import Navbar from "./Components/Navbar";
-import Home from "./Pages/Home";
-
-
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-     <Home />
     <div className="App">
       <Navbar />
      <Home />
